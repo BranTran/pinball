d725cd6941914967f41000add0eaef1faf85644a
Bran Tran <tran2833@stthomas.edu>
2019-05-02 12:26:25 -0500

    Initial server side things

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 var path = require("path");
 var express = require("express");//Don't need HTTP, FS, URL requires
-var sqlite3 = require("sqlite3");
+//var sqlite3 = require("sqlite3");
 
 
 var app = express();//Create an express app
 var port = 8010;
 
-var db_filename = path.join(__dirname, "db", "imdb.sqlite3");//Where server code is and path to file
+//var db_filename = path.join(__dirname, "db", "imdb.sqlite3");//Where server code is and path to file
 var public_dir = path.join(__dirname, "public");
 
-var db = new sqlite3.Database(db_filename, sqlite3.OPEN_READONLY, (err) => {
+/*var db = new sqlite3.Database(db_filename, sqlite3.OPEN_READONLY, (err) => {
 	if(err){
 		console.log("Error opening "+ db_filename);
 	}
@@ -19,9 +19,9 @@ var db = new sqlite3.Database(db_filename, sqlite3.OPEN_READONLY, (err) => {
 		//We worry about asycn issues, but in the end
 		//The chances of an issue are so small
 	}
-});
+});//*/
 
 app.use(express.static(public_dir));//Handles all of our requests
 
 var server = app.listen(port);
-
+console.log("Now listening on port "+port);
